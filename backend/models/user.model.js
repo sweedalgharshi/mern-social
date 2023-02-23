295ee9f9786ad625fa5b923fa64071b26740c320
sweedalgharshi <sweedalgracius@gmail.com>
2023-02-23 16:51:51 +0530

    created user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,50 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: [true, "Email already exists"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minLength: [6, "Password must be atleast 6 characters"],
+    select: false,
+  },
+
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
 const userModel = mongoose.model("User", userSchema);
 
