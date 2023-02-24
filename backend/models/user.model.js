68de1d7b25c47965500f5667b25b33acadf25898
sweedalgharshi <sweedalgracius@gmail.com>
2023-02-24 15:48:33 +0530

    bcrypted the password

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -46,6 +47,12 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 
+const User = mongoose.model("User", userSchema);
 module.exports = User;
