b123c66a32a030057cf8df9b3fc5ffb26dcfcb5c
sweedalgharshi <sweedalgracius@gmail.com>
2023-02-24 14:24:19 +0530

    added code in post.controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,21 @@ const Post = require("../models/post.model");
 
 async function createPost(req, res) {
   try {
+    const newPostData = {
+      caption: req.body.caption,
+      image: {
+        public_id: "req.body.public_id",
+        url: "req.body.url",
+      },
+      owner: req.user.id,
+    };
+
+    const newPost = await Post.create(newPostData);
+
+    res.status(201).json({
+      success: true,
+      post: newPost,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
